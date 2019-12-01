f871fa15b0a1b9c1b9e5153801f63bb8c45c07f8
User <swetowet@gmail.com>
2019-12-01 23:26:00 +0700

    HomeWorck2/1

---
+++ app/src/main/java/com/example/materialdesignhomeworck/ShetActivity.java
@@ -0,0 +1,14 @@
+package com.example.materialdesignhomeworck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_sheet);
+    }
+}
